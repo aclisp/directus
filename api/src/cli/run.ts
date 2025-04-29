609b35554a0bc84661fc175f00808cf247c7b546
huanghao <huanghao@youxin.cloud>
2025-04-29 14:21:44 +0800

    Add global-agent as a devDependency to use Google openid auth

--- api/src/cli/run.ts
+++ api/src/cli/run.ts
@@ -1,4 +1,15 @@
+import { ProxyAgent, setGlobalDispatcher } from 'undici';
+import { useLogger } from '../logger/index.js';
 import { createCli } from './index.js';
+
+const proxy = process.env['http_proxy'] || process.env['HTTP_PROXY'];
+
+if (proxy) {
+	const logger = useLogger();
+	logger.debug(`Enabling global proxy (${proxy}) for undici...`);
+	const proxyAgent = new ProxyAgent(proxy);
+	setGlobalDispatcher(proxyAgent);
+}
 
 createCli()
 	.then((program) => program.parseAsync(process.argv))
